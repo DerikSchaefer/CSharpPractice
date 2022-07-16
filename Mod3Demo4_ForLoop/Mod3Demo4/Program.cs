90345befda11b244de68a2d85980bc773880b6de
Derik Schaefer <72780230+DerikSchaefer@users.noreply.github.com>
2022-07-16 10:10:43 -0700

    Add files via upload

---
+++ Mod3Demo4_ForLoop/Mod3Demo4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod3Demo4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int sum = 0; 
+            for (int i = 0; i <= 5; i++)
+            {
+              Console.WriteLine("Please enter a number");
+              int number =  Int32.Parse(Console.ReadLine());
+              sum += number;  
+            }
+            Console.WriteLine($"The sum of the first 5 natural numbers is {sum}");
+            Console.ReadLine();
+            // This loop will take in 5 numbers from a user and add them together and return the sum
+        }
+    }
+}
