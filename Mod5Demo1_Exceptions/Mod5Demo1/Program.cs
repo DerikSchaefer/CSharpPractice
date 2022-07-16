fed359509f52ac446d5b067787e978ced32ac846
Derik Schaefer <72780230+DerikSchaefer@users.noreply.github.com>
2022-07-16 10:12:26 -0700

    Add files via upload

---
+++ Mod5Demo1_Exceptions/Mod5Demo1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod5Demo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        input: int num1,
+        num2;
+            Console.WriteLine("Please enter 2 whole numbers");
+            bool flag1 = Int32.TryParse(Console.ReadLine(), out num1);
+            bool flag2 = Int32.TryParse(Console.ReadLine(), out num2);
+            int result;
+            try
+            {
+                if (flag1 == true && flag2 == true)
+                {
+                    result = (num1 / num2);
+
+                    Console.WriteLine($"Result is {result}");
+                }
+                else
+                {
+                    Console.WriteLine("Please enter valid numbers");
+                    goto input;
+                }
+
+            }
+            catch (ArithmeticException ex) // more specific 
+            {
+                //handle error/ exception
+                Console.Write(ex.Message);
+                goto input;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                goto input;
+            }
+            finally
+            {
+                // mandatory code, file closing, db connection closing. 
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
