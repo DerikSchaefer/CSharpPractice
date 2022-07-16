aa506b5ec96904730d4d14f71fc072ce13e6875d
Derik Schaefer <72780230+DerikSchaefer@users.noreply.github.com>
2022-07-16 10:12:09 -0700

    Add files via upload

---
+++ Mod4Demo4_DataStructures/Mod4Demo4_DataStructures/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic; // improved namespace
+using System.Collections; // previous namespace
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod4Demo4_DataStructures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //array limitation : cannot dynamically grow
+
+              
+            // net 2.0 came out with ArrayList
+          // This is an array that's size is dynamically increased as required. 
+          ArrayList arrayList = new ArrayList(); // ref type
+
+            arrayList.Add(3);
+            arrayList.Add(4.5);
+            arrayList.Add("Derik");
+            //ArrayLists were not considered a good solution because they are not memory efficient. You are boxing and unboxing data
+            //everytime. There are better collections that have come out since then
+            foreach (object obj in arrayList)
+            {
+                Console.WriteLine(obj);
+            }
+            Stack stack = new Stack(3); // boxing and unboxing. 
+            stack.Push(3); // Don't use stacks either
+
+            List<int> list = new List<int>();  // When you see these angle brackets < > these are generics. These are the 'generics' 
+            //which is what everyone is using nowadays. Don't use collections that don't have these angle brackets. 
+            list.Add(3);    
+
+            Console.ReadLine();
+        }
+    }
+}
