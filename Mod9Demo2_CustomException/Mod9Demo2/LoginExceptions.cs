0d607d5a00fb5078a9af72a36c495f2c7b91dfb0
Derik Schaefer <72780230+DerikSchaefer@users.noreply.github.com>
2022-07-19 11:02:30 -0700

    Add files via upload

---
+++ Mod9Demo2_CustomException/Mod9Demo2/LoginExceptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod9Demo2
+{
+    internal class LoginExceptions:Exception
+
+    {
+
+        // lambda expression : => goes to
+
+        // We are overriding the message property from the base class (Exception is the base class). 
+        // This is a virtual class, we know this because we did not get an error when we invoked it. 
+        public override string Message => $"Invalid login attempt at {DateTime.Now} ";
+
+
+    }
+}
---
+++ Mod9Demo2_CustomException/Mod9Demo2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Mod9Demo2
+{
+    internal class Program
+    {
+
+        const string Path = "C:\\Files\\log.text";
+        static void Main(string[] args)
+        {
+
+            string username, password;
+            Console.WriteLine("Enter username:");
+            username = Console.ReadLine();
+            Console.WriteLine("Enter password:");
+            password = Console.ReadLine();
+
+            try
+            {
+                if (username == "Admin" && password == "password")
+                {
+                    Console.WriteLine("Successfull attempt!");
+                }
+                else
+                    // this is the login exception that we created on the other .cs file. We are able to throw this because we inherited it from
+                    // the base exception class
+                    throw new LoginExceptions();
+            }
+            catch (LoginExceptions ex) // Temporary variable created when we catch this variable. We do not have to define this any other way
+
+            {
+                Console.Beep(50, 500);
+                Console.WriteLine(ex.Message); // This 'Message' is a property of the LoginException class. 
+                AppendLog(ex.Message);
+
+            }
+            Console.ReadLine();
+            Console.WriteLine("Logs so far...");
+
+        }
+
+        static void ReadLogs()
+        {
+
+            StreamReader streamReader = new StreamReader(Path);
+            string data = streamReader.ReadToEnd();
+            Console.WriteLine(data);
+            streamReader.Close();
+        }
+
+        static void AppendLog(string data)
+        {
+            File.AppendAllText(Path, data + "\n");
+
+        }
+    }
+}
