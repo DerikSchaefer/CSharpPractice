2cd66bb96096c24d816d4ba137928bfc68413af0
Derik Schaefer <72780230+DerikSchaefer@users.noreply.github.com>
2022-07-19 15:21:04 -0700

    Add files via upload

---
+++ Mod9Demo3_Directory/Mod9Demo3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Mod9Demo3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DriveInfo driveInfo = new DriveInfo(@"C:\");
+            Console.WriteLine($"Drive info for C drive: {driveInfo.Name}, free space {driveInfo.TotalFreeSpace}");
+            Console.ReadLine();
+            DirectoryInfo[] directoryInfos = new DirectoryInfo(@"C:\").GetDirectories();
+          
+/*            foreach (DirectoryInfo directoryInfo in directoryInfos)
+            {
+                if (directoryInfo.Name.Length != 0)
+                {
+                    directoryInfo.Delete(true);
+                }*/
+                Console.WriteLine(directoryInfo.Name + " " + directoryInfo.CreationTime);
+                
+            }
+        }
+    }
+}
---
+++ TipCalculator/TipCalculator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace TipCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double tipRate = 0.18;
+            double billTotal = 0;
+
+            if (args.Length == 0 || !Double.TryParse(args[0], out billTotal))
+            {
+                Console.WriteLine("Please use :TipCalculator total");
+                Console.ReadLine();
+
+
+
+
+
+            }
+            double tip = billTotal * tipRate;
+            Console.WriteLine();
+            Console.WriteLine($"Your bill total is:\t {billTotal,8:c}");
+            Console.WriteLine($"Your tip total/rate:\t{tip,8:c} ({tipRate:p1})");
+            Console.WriteLine(("").PadRight(24, '-'));
+            Console.WriteLine($"Grand Total:\t{billTotal + tip, 8:c}");
+            Console.ReadLine();
+
+            #region formattingCurrency
+            /*            Console.WriteLine("(C) Currency:....{0:C}", 50); // This willl display a currency
+                        Console.WriteLine("(P) Percentage:....{0:P}", 50); // this will display a percentage
+                        double val = 34343.34;
+                        Console.WriteLine(val.ToString("C", CultureInfo.CreateSpecificCulture("en-GB"))); // Displays in British pounds
+                        Console.ReadLine(); */
+            #endregion
+
+        }
+    }
+}
