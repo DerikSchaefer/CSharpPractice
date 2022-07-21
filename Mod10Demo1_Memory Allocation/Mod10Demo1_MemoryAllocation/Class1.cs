28d1f01ee6fc10bc3180b31a4e61a6508737dade
Derik Schaefer <72780230+DerikSchaefer@users.noreply.github.com>
2022-07-21 08:04:43 -0700

    Add files via upload

---
+++ Mod10Demo1_Memory Allocation/Mod10Demo1_MemoryAllocation/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod10Demo1_MemoryAllocation
+{
+    internal class ClassTest // heap, ref types
+    {
+        public int i;
+    }
+
+    struct StructTest // value types, stack.
+    {
+        public int i;
+    }
+}
