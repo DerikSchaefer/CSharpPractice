801cddba72503a3ca04b228c1a65f50942165d04
Derik Schaefer <72780230+DerikSchaefer@users.noreply.github.com>
2022-07-21 08:02:54 -0700

    Add files via upload

---
+++ Assingment2.2/Assignment2.2/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+/*1.Write a console application to create a text file and save your basic details like name, age, 
+    address ( use dummy data). Read the details from same file and print on console.*/
+namespace Assignment2._2
+{
+    internal class Program
+    {
+
+        // Create a variable to store our filename
+
+        // const for the path
+
+        // We declare these two variables in the class / outside the main method because this way we can 
+        //use these variables outside the main if needed. 6
+        static string filename;
+        const string path = "C:\\Files\\";
+        static void Main(string[] args)
+        {
+
+        //Ask the user for their name, this will be what we name the file 
+            Console.WriteLine("Please enter your name");
+
+        //Store the input from the user in a variable
+            string name = Console.ReadLine();
+
+        //Combine the input from the user + ".text"
+            filename = name + ".text";
+
+        //Combine this all into a full file name
+            string temp = path + filename;
+
+        //Create an instance of the streamWriter object, this is from the System.IO class
+            StreamWriter streamWriter = null;
+
+
+        //Create a try block
+            try
+            {
+
+        // We're going to check if the file already exists, if it does not we will create it
+                if (!File.Exists(temp))
+                {
+                    streamWriter = File.CreateText(temp);
+                }
+
+        //If it does already exist we're going to delete the old one and recreate a new one
+                else
+                {
+                    streamWriter = new StreamWriter(temp);
+                }
+
+        //We will use the value stored in the name variable from earlier to write the user's name to the
+        //file we created
+                streamWriter.WriteLine(name);
+
+        //Ask for the user's age and address, store these in variables and then write these to the file
+        //using the WriteLine method
+                Console.WriteLine("What is your age");
+                string age = Console.ReadLine();
+                streamWriter.WriteLine(age);
+                Console.WriteLine("What is your address");
+                string address = Console.ReadLine();
+                streamWriter.WriteLine(address);
+            }
+        //Catch block
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        //Finally block, since streamWriter will not be null this will close out this block
+            finally
+            {
+                if (streamWriter != null)
+                {
+                    streamWriter.Close();
+                }
+            }
+
+        //Ask the user if they would like to see their data
+            Console.WriteLine("Would you like to see your data? Y/N");
+
+        //Store this in a temp variable and use an if statement to check their answer
+            string answer = Console.ReadLine();
+            if (answer == "Y")
+            {
+        //If they answered yes we will use a while loop to iterate through every line of the file and 
+        //return those back to the user via the console.
+                using (StreamReader sr = new StreamReader(temp))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+        //Console ReadLine to pause everything
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Mod10Demo1_Memory Allocation/Mod10Demo1_MemoryAllocation/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod10Demo1_MemoryAllocation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num1, num2;
+            num1 = 100;
+            num2 = 200;
+
+            Swap(ref num1, ref num2); // Function call
+
+            // This does not work and swap the actual values because just a copy was made
+        /*    Console.WriteLine($"Swapped values are num1:{num1} & num2 {num2}");
+            Console.ReadLine();
+*/
+   /*         add(num1, num2);
+*/
+            
+/*
+            Console.WriteLine($"Swapped values are num1:{num1} & num2 {num2}");
+            Console.ReadLine();*/
+
+
+
+            StructTest structTest = new StructTest();
+            structTest.i = 1;
+            TestStruct(structTest);
+            Console.WriteLine($"value of i from struct: {structTest.i}");
+            Console.ReadLine();
+
+
+            // If you passed this as TestStruct(ref structTest) it as well as in the function it would work to
+            //change the actual value
+
+
+
+            ClassTest classTest = new ClassTest();
+            classTest.i = 1;
+            TestClass(classTest);
+            Console.WriteLine($"value of i from class: {classTest.i}");
+            Console.ReadLine();
+
+
+
+
+
+
+        }
+
+
+
+        // You would not need to use ref here because you're not trying to change the values of the 
+        // variables passed as arguments, you're only trying to return the value of the operation.
+
+
+
+
+        /*  static int add(int i, int j)
+          {
+              i = i * 2;
+              return i - j;
+          }
+  */
+
+        static void TestClass(ClassTest obj)
+        {
+            obj.i = 100;
+        }
+        
+        static void TestStruct(StructTest obj)
+        {
+            obj.i = 100;
+            Console.WriteLine($"Value of i {obj.i}");
+        }
+        
+        
+        static int Add(int i, int j)
+        {
+            return i + j;
+        }
+
+        static void Swap(ref int i, ref int j) // value types before the ref keyword was added. The ref
+            //keyword points to the address of the actual variable now 
+        {
+            int temp;
+
+            temp = i; 
+            i = j; 
+            j = temp;
+
+        }
+
+
+
+
+    }
+}
