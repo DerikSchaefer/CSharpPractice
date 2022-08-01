06b617eff445316622150d470ee0cce8c242c191
Derik Schaefer <72780230+DerikSchaefer@users.noreply.github.com>
2022-08-01 15:53:32 -0700

    Add files via upload

---
+++ Wk4Mod1Demo3_DateTime/Mod1Demo3_DateTime/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod1Demo3_DateTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter your date of birth in yyyy/mm/dd format: ");
+            var dob = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine($"You are {CalculateAge(dob)} years old");
+            Console.ReadLine();
+        }
+
+        static int CalculateAge(DateTime dateOfBirth)
+        {
+            int age = DateTime.Now.Year - dateOfBirth.Year;
+            int months = DateTime.Now.Month - dateOfBirth.Month;
+            return age;
+        }
+
+        
+    }
+}
