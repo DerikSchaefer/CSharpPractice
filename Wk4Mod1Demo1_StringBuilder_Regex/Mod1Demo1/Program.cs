7f125040e4cdf27ca659b3b7c524fe85197d5ad4
Derik Schaefer <72780230+DerikSchaefer@users.noreply.github.com>
2022-08-01 12:34:57 -0700

    Add files via upload

---
+++ Wk4Mod1Demo1_StringBuilder_Regex/Mod1Demo1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod1Demo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num = 23;  // explicity typed
+            var num1 = 100;
+            var name = "Deepali";
+            var rate = 3.4f; // becomes float
+
+
+            string[] myArray = { "apple", "banana", "grapes" };
+
+            var result = from fruit in myArray
+                         where fruit[0] == 'g'
+                         select fruit;
+
+            Console.WriteLine("Fruits starting with G");
+            foreach (var fruit in myArray)
+            {
+                Console.WriteLine(fruit);
+            }
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ Wk4Mod1Demo1_StringBuilder_Regex/Mod1Demo2StringDemo/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+namespace Mod1Demo2StringDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+/*
+            // string concatenation: not efficient, creates this many strings. 
+            string address;
+            address = "27";
+            address = address + " miller street";
+            address = address + " NY";
+            Console.WriteLine(address);
+            Console.ReadLine();
+
+            // String Builder is more efficient
+            // Stringbuilder is a class included in the .net framework. We're creating an 
+            // variable of this class (an object) and then using the .Append method and then passing
+            // arguments to that method.
+            StringBuilder newAddress = new StringBuilder();
+            newAddress.Append("45");
+            newAddress.Append(" lincoln blvd");
+            newAddress.Append(" NJ");
+            Console.WriteLine(newAddress.ToString());
+            Console.ReadLine();
+
+            string myAddress = newAddress.ToString();
+
+            var reverse = myAddress.Reverse<char>();
+
+            foreach (char c in reverse)
+            {
+                Console.Write(c);
+                Console.ReadLine();
+
+            }
+
+*/
+
+        
+
+
+            int count = 0;
+            /*  foreach (var c in newAddress.ToString()) 
+              {
+                  if (c == ' ')
+                  {
+                      count++;
+                  }
+                  Console.WriteLine(count);
+                  Console.ReadLine();*/
+
+            Console.WriteLine(CheckPhoneNumber("+23 578-045-888")); // true
+
+            Console.WriteLine(CheckPhoneNumber("+444 578-045-888")); // false
+            Console.WriteLine(CheckPhoneNumber("+00 578-045-888")); // false
+
+            Console.ReadLine();
+
+
+            Console.WriteLine("Please enter email id:");
+                string emailtocheck = Console.ReadLine();
+            if (CheckEmail(emailtocheck))
+            {
+                Console.WriteLine("Correct email");
+            }
+            else
+            {
+                Console.WriteLine("Error in email");
+            }
+        }
+
+
+        static bool CheckPhoneNumber(string phoneNumber)
+        {
+            Regex regex = new Regex(@"^\+\d{2}\s(\d{3}\-){2}(\d{3})");
+            Match match = regex.Match(phoneNumber);
+            return match.Success;
+        }
+
+
+        static bool CheckEmail(string email)
+        {
+          string emailpattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+            var result = Regex.Match(email, emailpattern);
+            return result.Success;
+        }
+    }
+}
+
+
+
