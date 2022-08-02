c488b0e7765e0ad8508023900d88d4db9fc91d5b
Derik Schaefer <72780230+DerikSchaefer@users.noreply.github.com>
2022-08-02 13:19:27 -0700

    Add files via upload

---
+++ Wk4Mod2Demo2_MethodOverloading/Wk4Mod2_OperatorOverloading/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wk4Mod2_OperatorOverloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // the + operator is overloaded, it can either concatenate strings or add numbers
+            int num = 5 + 6 + 8 + 9;
+
+            string name = "welcome" + " all";
+
+            circle c1 = new circle();
+            c1.Radius = 23;
+            circle c2 = new circle();
+            c2.Radius = 34;
+            var c3 = c1 + c2;
+            c3.CalculateArea();
+            Console.WriteLine($"Area of third circle is {c3.area}, the radius is {c3.Radius}");
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Wk4Mod2Demo2_MethodOverloading/Wk4Mod2_OperatorOverloading/circle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wk4Mod2_OperatorOverloading
+{
+    internal class circle
+    {
+
+        public double Radius { get; set; }
+        public double area { get; set; }
+
+
+        // This calculate area method isn't used as part of the operator overloading
+        public void CalculateArea()
+        {
+            this.area = Math.PI * this.Radius * this.Radius;
+        }
+
+        // This is an example of operator overloading, we added a new functionality to the + operation
+
+        // This 'circle' in the name is in reference to the return type from this method which will 
+        // return an object of the circle class. The 'name' of this method is operator + 
+        public static circle operator +(circle c1, circle c2)
+        {
+            circle cnew = new circle();
+            cnew.Radius = c1.Radius + c2.Radius;
+            return cnew;
+        }
+
+        public static circle operator -(circle c1, circle c2)
+        {
+            circle newCircle = new circle();
+            newCircle.Radius = c1.Radius - c2.Radius;
+            return newCircle;
+        }
+    }
+}
