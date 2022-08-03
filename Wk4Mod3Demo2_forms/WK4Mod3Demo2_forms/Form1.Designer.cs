661e9e7207ead4cdf9bd99b3e096b4f9906e7b04
Derik Schaefer <72780230+DerikSchaefer@users.noreply.github.com>
2022-08-03 12:34:19 -0700

    Add files via upload

---
+++ Wk4Mod3Demo2_forms/WK4Mod3Demo2_forms/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace WK4Mod3Demo2_forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.BtnHello = new System.Windows.Forms.Button();
+            this.txtName1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(427, 153);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login Form xyz";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // BtnHello
+            // 
+            this.BtnHello.Location = new System.Drawing.Point(408, 309);
+            this.BtnHello.Name = "BtnHello";
+            this.BtnHello.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.BtnHello.Size = new System.Drawing.Size(75, 23);
+            this.BtnHello.TabIndex = 1;
+            this.BtnHello.Text = "Press me";
+            this.BtnHello.UseVisualStyleBackColor = true;
+            this.BtnHello.Click += new System.EventHandler(this.BtnHello_Click);
+            // 
+            // txtName1
+            // 
+            this.txtName1.Location = new System.Drawing.Point(258, 177);
+            this.txtName1.Name = "txtName1";
+            this.txtName1.Size = new System.Drawing.Size(100, 20);
+            this.txtName1.TabIndex = 2;
+            this.txtName1.TextChanged += new System.EventHandler(this.txtName1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtName1);
+            this.Controls.Add(this.BtnHello);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtnHello;
+        private System.Windows.Forms.TextBox txtName1;
+    }
+}
+
---
+++ Wk4Mod3Demo2_forms/WK4Mod3Demo2_forms/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WK4Mod3Demo2_forms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            // Event handler. What you want to do when the form gets loaded is decided here 
+            MessageBox.Show("Welcome to Winforms!");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnHello_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello");
+            label1.Text = "Welcome! " + txtName1.Text;
+        }
+
+        private void txtName1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
