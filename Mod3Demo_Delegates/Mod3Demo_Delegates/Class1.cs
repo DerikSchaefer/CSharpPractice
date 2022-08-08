dfbbb1df0be7212112acdda907d98e704d0f7421
Derik Schaefer <72780230+DerikSchaefer@users.noreply.github.com>
2022-08-08 12:56:56 -0700

    Add files via upload

---
+++ Mod3Demo_Delegates/Mod3Demo_Delegates/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod3Demo_Delegates
+{
+    // delegate : a function pointer
+
+    // this mydel can point to functions hich follow the signature: return void 
+    public delegate void MyDel(string s); 
+    internal class Class1
+    {
+    }
+}
---
+++ Mod3Demo_Delegates/Mod3Demo_Delegates/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod3Demo_Delegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDel del = new MyDel(Sayhello);
+            del("all"); // calling the function through the delegate
+            MyDel myDel = new MyDel(SayHi);
+            Greetings(del);
+            Greetings(myDel);
+        }
+            public static void Sayhello(string s)
+            {
+                Console.WriteLine("Hello " + s);
+            }
+
+        public static void SayHi(string a)
+        {
+            // logic
+            Console.WriteLine("Hi " + a);
+        }
+
+        static void Greetings(MyDel my)
+        {
+            my("all");
+        }
+        }
+    }
+
