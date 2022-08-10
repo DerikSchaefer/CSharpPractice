509ae04f6f958c73dc1dfd63d481d1ae66e87246
Derik Schaefer <72780230+DerikSchaefer@users.noreply.github.com>
2022-08-10 15:11:02 -0700

    Add files via upload

---
+++ DictionaryPT2/DictionaryPT2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryPT2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Dictionary<string, string> Recipies = new Dictionary<string, string>();
+            Recipies.Add("Fried Rice", "quick fried rice");
+            Recipies.Add("Stir Fry", "Make a stir-fry!");
+
+            foreach (var name in Recipies)
+            {
+                Console.WriteLine(name);
+            }
+            
+           Console.WriteLine("Enter the recipie to search:");
+            string rec_name = Console.ReadLine();
+            if(Recipies.ContainsKey(rec_name))
+            {
+                Console.WriteLine(Recipies[rec_name]);
+            }
+            Console.ReadLine();
+           
+
+        }
+    }
+}
