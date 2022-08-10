62886eddc559bb0351dd1341be03740f003e6e04
Derik Schaefer <72780230+DerikSchaefer@users.noreply.github.com>
2022-08-10 15:10:39 -0700

    Add files via upload

---
+++ Mod4Demo5_Generics/GenericsDemo/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsDemo
+{
+
+    // Generic class to do swapping of any data type
+class Swap<T> // T: placeholder for the datatype 
+    {
+
+        // members 
+        public void SwapData(ref T num1, ref T num2)
+        {
+            T temp;
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+
+
+
+        }
+    }
+}
---
+++ Mod4Demo5_Generics/GenericsDemo/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // This is us creating an instance of our custom
+            // generic class we created in the class1.cs file
+            // replacing the data type with 'int'. We can also do this 
+            // with any other datatype
+         
+            int i = 1; 
+            int j = 2;
+            Swap<int> swapInt = new Swap<int>();
+            swapInt.SwapData(ref i, ref j);
+
+            // We can do this with a float too. 
+            Swap<float> swapFloat = new Swap<float>();
+
+
+            Queue<int> queue = new Queue<int>(); // FIFO
+            // enqueue and dequeue 
+            Stack<int> stack = new Stack<int>(); // LIFO
+            // push and pop
+
+            SortedDictionary<string, car> carDictionary = new SortedDictionary<string, car>();
+
+            // This is the key, the vin                    // this is the value(s)
+            carDictionary.Add("Z1234F", new car() { VIN = "Z1234F", Make = "BMW   ", Model = "3Series", Price = 123451 });
+            carDictionary.Add("Z1234D", new car() { VIN = "Z1234D", Make = "VW    ", Model = "Bug" });
+            carDictionary.Add("Z1234E", new car() { VIN = "Z1234E", Make = "Mazda ", Model = "3" });
+            carDictionary.Add("Z1234G", new car() { VIN = "Z1234G", Make = "Suburu", Model = "Imprezza" });
+            carDictionary.Add("Z1234A", new car() { /*VIN = "Z1234A",*/ Make = "Ford  ", Model = "F150" });
+
+            //Console.WriteLine(carDictionary.ContainsKey("Z1234E"));
+          
+            car car4 = new car() { VIN = "CDD4G", Make = "Honda", Model = "CRV", Price = 1234123};
+            carDictionary.Add(car4.VIN, car4);
+
+            foreach (var car in carDictionary.Values)
+            {
+                Console.WriteLine($"Car VIN :{car.VIN} || Make :{car.Make} || Model : {car.Model}");
+            }
+            Console.WriteLine("Enter the VIN to search the car:");
+            string VIN = Console.ReadLine();
+            if (carDictionary.ContainsKey(VIN))
+            {
+                Console.WriteLine($"{carDictionary[VIN].Model} , { carDictionary[VIN].Make} , {carDictionary[VIN].Price}");
+            }
+            else
+            {
+                Console.WriteLine("No match found");
+            }
+            Console.ReadLine();
+
+            foreach (var car2 in carDictionary)
+            {
+                Console.WriteLine(car2);
+            }
+            Console.ReadLine();
+        }
+    }
+}
