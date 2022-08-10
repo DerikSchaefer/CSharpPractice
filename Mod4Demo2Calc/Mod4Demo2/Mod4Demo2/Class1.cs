d81de0bb256928c25b9cb1d6ba40fe173817e0e7
Derik Schaefer <72780230+DerikSchaefer@users.noreply.github.com>
2022-08-10 15:09:59 -0700

    Add files via upload

---
+++ Mod4Demo2Calc/Mod4Demo2/Mod4Demo2/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod4Demo2
+{
+    interface ICalculator
+    {
+        int Add(int i, int j);
+        int Multiply(int i, int j);
+        int Divide(int i, int j);
+        int Subtraction(int i, int j);
+    }
+    //interface ICalculator2
+    //{
+    //    int Add(int i,int j);
+    //}
+    class MathCls : ICalculator //ICalculator2
+    {
+        // implicit implementation
+
+
+        // explicit implementation
+        int ICalculator.Add(int i, int j)
+        {
+            return i + j;
+           
+        }
+
+        
+
+        int ICalculator.Divide(int i, int j)
+        {
+            throw new NotImplementedException();
+        }
+
+        int ICalculator.Multiply(int i, int j)
+        {
+            return i * j;
+           
+        }
+
+        int ICalculator.Subtraction(int i, int j)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ Mod4Demo2Calc/Mod4Demo2/Mod4Demo2/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mod4Demo2
+{
+    public partial class Form1 : Form
+    {
+        ICalculator calculator;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           MessageBox.Show(calculator.Add(Int32.Parse(textBox1.Text),Int32.Parse(textBox2.Text)).ToString());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            calculator = new MathCls();
+        }
+    }
+}
