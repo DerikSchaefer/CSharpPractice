9e962198203b7ee6903286850f548bd0b9acfa92
Derik Schaefer <72780230+DerikSchaefer@users.noreply.github.com>
2022-08-10 15:11:33 -0700

    Add files via upload

---
+++ ExentensionMethods/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace ExentensionMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "test000";
+            s.ContainsNumbers(); // useability
+            s.Contains("test");
+            ContainsNumbers(s);
+            Console.WriteLine(s); 
+        }
+
+        public static bool ContainsNumbers(string s) // extending string functionality
+        {
+            return Regex.IsMatch(s, @"\d");
+        }
+    }
+}
