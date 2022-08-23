6f5338469c805f78330ed5479ef7f4be460725c0
Derik Schaefer <72780230+DerikSchaefer@users.noreply.github.com>
2022-08-23 08:29:55 -0700

    Add files via upload

---
+++ Wk7CrudDLL/CRUD_dll/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD_dll
+{
+
+
+    internal class Student 
+    {
+
+
+    }
+
+  interface CRUD
+    {
+
+        void AddRecord(Student student);
+        void DeleteRecord(Student student);
+        void UpdateRecord(int recordId, Student student);
+        IEnumerable<Student> GetAll();
+
+
+    }
+
+    public class StudentRepository : CRUD
+    {
+        void CRUD.AddRecord(Student student)
+        {
+            throw new NotImplementedException();
+        }
+
+        void CRUD.DeleteRecord(Student student)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<Student> CRUD.GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        void CRUD.UpdateRecord(int recordId, Student student)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
