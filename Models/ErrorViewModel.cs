17b999cf05ff90661dea03caf1a61c810129f3f4
Derik Schaefer <72780230+DerikSchaefer@users.noreply.github.com>
2022-09-14 10:21:19 -0700

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Mod1Demo1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
