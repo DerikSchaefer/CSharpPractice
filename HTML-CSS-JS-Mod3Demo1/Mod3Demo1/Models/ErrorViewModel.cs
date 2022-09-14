7f176ded4d6b0afe6fc7a5205fce4b5d01633c30
Derik Schaefer <72780230+DerikSchaefer@users.noreply.github.com>
2022-09-14 10:45:10 -0700

    Add files via upload

---
+++ HTML-CSS-JS-Mod3Demo1/Mod3Demo1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mod3Demo1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
